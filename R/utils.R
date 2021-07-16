4b728ae5fdec12d7245b38779f3da6fd08d13557
Seungchan Kim <dolchan@gmail.com>
2021-07-15 23:06:31 -0500

    split_se (utils.R) added

---
+++ R/utils.R
@@ -0,0 +1,27 @@
+#' Split SummarizedExperiment object
+#'
+#' @param se SummarizedExperiment
+#' @param col integer (index) or string (column heading) of colData(se)
+#' @return a list of split objects
+split_se <- function(se, col) {
+  cx <- unique(se[[col]])
+
+  res <- lapply(cx, function(cc) {
+    idx <- which(se[[col]] == cc)
+    si <- se[, idx]
+    metadata(si) <- subset_metadata(se, idx)
+    si
+  })
+
+  names(res) <- cx
+  res
+}
+
+#' Subset metadata
+#' @noRd
+subset_metadata <- function(se, idx) {
+  mse <- metadata(se)
+  res <- lapply(mse, function(df) df[idx, ])
+  names(res) <- names(mse)
+  res
+}
